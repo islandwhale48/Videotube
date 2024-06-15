28bd206194634cb4c29186f2b220ec3acc58eba5
islandwhale48 <anupriya87090@gmail.com>
2024-06-15 08:53:12 +0530

    here are git comd line pushes and commits

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-
+import { db_name } from '../constants.js'
 const connectdb = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${db_name}`)
-        console.log("connection Successful", connectionInstance);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${db_name}`)
+        console.log(" hey anu the database connection is Successful at", connectionInstance.connection.host);
     }
     catch (error) {
-        console.log("error occured", error);
+        console.log(" hey anu A MONGODB error occured", error);
         process.exit(1);
 
     }
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import connectdb from './db/index.js';
+import app from './app.js';
+dotenv.config({ path: '../env' });
+
+
+connectdb()
+    .then(() => {
+
+        app.listen(process.env.PORT || 4000, (req, res) => {
+            console.log("oh boy! Here we go....")
+            console.log("Connection Successful at", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        app.on('error', (err) => {
+            console.log("express doesn't work, you gotta look into it!")
+        })
+        console.log("connection failed")
+    })
